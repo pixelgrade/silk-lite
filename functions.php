061acb249404fee042d88a5e1846e30bdb1d7a9a
Razvan Onofrei <razwan.me@gmail.com>
2015-01-15 16:52:39 +0200

    sonnet initialization

--- functions.php
+++ functions.php
@@ -102,10 +102,6 @@ add_action( 'widgets_init', 'sonnet_widgets_init' );
 function sonnet_scripts() {
 	wp_enqueue_style( 'sonnet-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'sonnet-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'sonnet-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
--- inc/customizer.php
+++ inc/customizer.php
@@ -20,7 +20,7 @@ add_action( 'customize_register', 'sonnet_customize_register' );
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
-function sonnet_customize_preview_js() {
-	wp_enqueue_script( 'sonnet_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
-}
-add_action( 'customize_preview_init', 'sonnet_customize_preview_js' );
+// function sonnet_customize_preview_js() {
+// 	wp_enqueue_script( 'sonnet_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
+// }
+// add_action( 'customize_preview_init', 'sonnet_customize_preview_js' );
