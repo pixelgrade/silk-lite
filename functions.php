96836ae3a2d9708b4af3f3ba8c66a6a80065686e
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-01-22 22:48:22 +0200

    changed widgets title from h1 to h2

--- functions.php
+++ functions.php
@@ -107,21 +107,21 @@ function swell_widgets_init() {
 	register_sidebar( array(
 		'name'          => __( 'Sidebar', 'swell_txtd' ),
 		'id'            => 'sidebar-1',
-		'description'   => '',
+		'description'   => __( 'Add widgets here to appear in your main sidebar.', 'swell_txtd' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 
 	register_sidebar( array(
 		'name'          => __( 'Footer', 'swell_txtd' ),
 		'id'            => 'footer-1',
-		'description'   => '',
+		'description'   => __( 'Add widgets here to appear in your footer sidebar.', 'swell_txtd' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
 	) );
 }
 
