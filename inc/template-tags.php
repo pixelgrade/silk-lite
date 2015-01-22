e8d2f4591d2f6637695ac2fa66f2d46640ea6c59
cristianfrumusanu <cristian.frumusanu@pixelgrade.com>
2015-01-22 16:49:30 +0200

    work for #10

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -37,13 +37,13 @@ function the_posts_navigation() {
 }
 endif;
 
-if ( ! function_exists( 'the_post_navigation' ) ) :
+//if ( ! function_exists( 'the_post_navigation' ) ) :
 /**
  * Display navigation to next/previous post when applicable.
  *
  * @todo Remove this function when WordPress 4.3 is released.
  */
-function the_post_navigation() {
+function swell_the_post_navigation() {
 	// Don't print empty markup if there's nowhere to navigate.
 	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
 	$next     = get_adjacent_post( false, '', false );
@@ -54,16 +54,30 @@ function the_post_navigation() {
 	?>
 	<nav class="navigation post-navigation" role="navigation">
 		<h2 class="screen-reader-text"><?php _e( 'Post navigation', 'swell_txtd' ); ?></h2>
-		<div class="nav-links">
-			<?php
-				previous_post_link( '<div class="nav-previous">%link</div>', '%title' );
-				next_post_link( '<div class="nav-next">%link</div>', '%title' );
-			?>
-		</div><!-- .nav-links -->
+		<div class="article-navigation">
+			<?php previous_post_link('<div class="navigation-item  navigation-item--previous">%link</div>',
+				sprintf('<span class="arrow"></span>
+                                                    <div class="navigation-item__content">
+                                                        <div class="navigation-item__wrapper">
+                                                            <span class="button-title">%s</span>
+                                                            <h3 class="post-title">%%title</h3>
+                                                        </div>
+                                                    </div>',
+					__('Previous Article', 'swell_txtd' )) ); ?>
+
+			<?php next_post_link('<div class="navigation-item  navigation-item--next">%link</div>',
+				sprintf('<span class="arrow"></span>
+                                                    <div class="navigation-item__content">
+                                                        <div class="navigation-item__wrapper">
+                                                            <span class="button-title">%s</span>
+                                                            <h3 class="post-title">%%title</h3>
+                                                        </div>
+                                                    </div>',
+					__('Next Article', 'swell_txtd' )) ); ?>
 	</nav><!-- .navigation -->
 	<?php
 }
-endif;
+//endif;
 
 if ( ! function_exists( 'swell_posted_on' ) ) :
 /**
