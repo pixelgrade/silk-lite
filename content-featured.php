b7b9d1dfaaf3936b8fd166cfba1023b99d1b20ac
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-01-26 16:03:42 +0200

    changed archive post titles to H2 and widget titles to H3

--- content-featured.php
+++ content-featured.php
@@ -27,7 +27,7 @@ if ( empty( $featured ) )
 					</span>
 				</a>
 				<header class="entry-header">
-					<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+					<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
 					<div class="entry-meta">
 						<?php amelie_posted_on(); ?>
--- home.php
+++ home.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			
+
 		<?php if ( amelie_has_featured_posts( ) ) : ?>
 			<div class="featured-content">
 				<?php get_template_part( 'content-featured' ); ?>
