482bd140a9c41f50ec3c5cf31b6f9430c138296d
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-04-20 13:01:46 +0300

    fixes #71

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'silk_txtd' ); ?></p>
+					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search.', 'silk_txtd' ); ?></p>
 
 					<?php get_search_form(); ?>
 
