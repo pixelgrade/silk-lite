087530c7d7ef532a919131b0dd12132e3a45d77f
Vlad Olaru <vlad.olaru@pixelgrade.com>
2015-05-07 11:56:18 +0300

    fixed VideoPress crashing Infinite Scroll

--- functions.php
+++ functions.php
@@ -208,6 +208,9 @@ function silk_scripts_styles() {
 		'velocity',
 	), '1.0.0', true );
 
+	$js_url = ( is_ssl() ) ? 'https://v0.wordpress.com/js/videopress.js' : 'http://s0.videopress.com/js/videopress.js';
+	wp_enqueue_script( 'videopress', $js_url, array( 'jquery', 'swfobject' ), '1.09' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
